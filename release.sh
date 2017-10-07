c1575d06d9add009bf723bb1a5f4b8282e883b07
Scott Kovatch <skovatch@users.noreply.github.com>
2017-10-06 20:37:12 -0700

    Reorganized the project for automatic building. Will try to use github built-in pages service.

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -v
+
+PROJECT_NAME="Get iPlayer Automator"
+PROJECT_DIR=$(pwd)
+INFOPLIST_FILE="Info.plist"
+
+CFBundleVersion=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+CFBundleShortVersionString=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "${PROJECT_DIR}/${INFOPLIST_FILE}")
+
+rm -rf Archive/*
+rm -rf Product/*
+
+xcodebuild clean -project "$PROJECT_NAME.xcodeproj" -configuration Release -alltargets
+
+xcodebuild archive -project "$PROJECT_NAME.xcodeproj" -scheme "$PROJECT_NAME" -archivePath "Archive/$PROJECT_NAME.xcarchive"
+
+xcodebuild -exportArchive -archivePath "Archive/$PROJECT_NAME.xcarchive" -exportPath "Product/$PROJECT_NAME" -exportOptionsPlist ExportOptions.plist
+
+cd "Product/${PROJECT_NAME}"
+zip -r "../$PROJECT_NAME.v${CFBundleShortVersionString}.b${CFBundleVersion}.zip" "$PROJECT_NAME.app"
